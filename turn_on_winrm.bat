747f8cd93fc3710f3f8cee0ae3e73e57c77f6fb8
Hippie Hacker <github@hippiehacker.org>
2013-04-02 10:51:38 -0700

    Create turn_on_winrm.bat

---
+++ turn_on_winrm.bat
@@ -0,0 +1,7 @@
+REM run me
+@echo on
+winrm quickconfig -q
+winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
+winrm set winrm/config @{MaxTimeoutms="1800000"}
+winrm set winrm/config/service @{AllowUnencrypted="true"}
+winrm set winrm/config/service/auth @{Basic="true"}
